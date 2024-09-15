c2ab355ab853d29ba745120ca246aa34762b75af
Ian Maurmann <ikmxx@yahoo.com>
2024-09-14 22:58:24 -0700

    Add examples for testing

---
+++ test-0003-colors.php
@@ -0,0 +1,28 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+require 'vendor/autoload.php';
+
+$writer = new \IKM\CLI\CommandLineWriter();
+$format = new \IKM\CLI\CommandLineFormatter();
+
+// ===========================================
+
+$writer->writeLine('ASCII Colors:');
+
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_red     . ' Dark Red     ' . $format->reset . ' ' . $format->bg_dark_red     . ' Dark Red     ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_green   . ' Dark Green   ' . $format->reset . ' ' . $format->bg_dark_green   . ' Dark Green   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_yellow  . ' Dark Yellow  ' . $format->reset . ' ' . $format->bg_dark_yellow  . ' Dark Yellow  ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_blue    . ' Dark Blue    ' . $format->reset . ' ' . $format->bg_dark_blue    . ' Dark Blue    ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_magenta . ' Dark Magenta ' . $format->reset . ' ' . $format->bg_dark_magenta . ' Dark Magenta ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
+$writer->writeLine('    ' . $format->fg_dark_black   . ' Dark Black   ' . $format->reset . ' ' . $format->bg_dark_black   . ' Dark Black   ' . $format->reset);
