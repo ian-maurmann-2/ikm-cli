622b8fe58ae3509311903caf63a023f2c614146e
Ian Maurmann <ikmxx@yahoo.com>
2024-09-15 19:05:44 -0700

    Test drawing shapes

---
+++ test-0005-draw.php
@@ -0,0 +1,268 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+require 'vendor/autoload.php';
+
+$writer = new \IKM\CLI\CommandLineWriter();
+$format = new \IKM\CLI\CommandLineFormatter();
+
+// ===========================================
+
+$writer->writeLine('⠿⣿');
+
+$writer->br();
+
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+$writer->writeLine('⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿');
+
+$writer->br();
+
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+
+$writer->br();
+
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿');
+$writer->writeLine('⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿');
+$writer->writeLine('⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿');
+$writer->writeLine('⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿');
+$writer->writeLine('⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿');
+$writer->writeLine('⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿');
+$writer->writeLine('⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿');
+$writer->writeLine('⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿');
+$writer->writeLine('⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+
+$writer->br();
+
+$writer->writeLine('Angle 1/1:');
+$writer->br();
+
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+
+$writer->br();
+
+$writer->writeLine('Angle 1/2:');
+$writer->br();
+
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⠀⠀⣼');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⣼⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀    ⠀⠀⠀⠀⣼⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⣼⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀  ⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀ ⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+
+$writer->br();
+$writer->writeLine('Angle 1/3:');
+$writer->br();
+
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⠀⠀⣠⣾');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⣠⣾⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+$writer->writeLine('⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿');
+
+$writer->br();
+
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣧⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀');
+$writer->writeLine('⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀');
+$writer->writeLine('⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀');
+$writer->writeLine('⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+
+$writer->br();
+
+$writer->writeLine('⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀');
+$writer->writeLine('⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀');
+$writer->writeLine('⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡟⠀⠀⠀⠀⠀⠀⠀⠀');
+
+$writer->br();
+
+
+
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣧⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀');
+$writer->writeLine('⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀');
+$writer->writeLine('⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀');
+$writer->writeLine('⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀');
+$writer->writeLine('⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀');
+$writer->writeLine('⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡟⠀⠀⠀⠀⠀⠀⠀⠀');
+
+$writer->br();
+
+
+
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⠀⠀⣼⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⣼⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀    ⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀  ⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀ ⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+
+$writer->br();
+
+$writer->writeLine('⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀ ⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀  ⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀    ⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⢻⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⠀⠀⢻⡟');
+
+$writer->br();
+
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⠀⠀⣼⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⣼⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀    ⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀  ⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀ ⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧');
+$writer->writeLine('⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀ ⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀  ⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀   ⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀    ⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⢻⣿⣿⣿⣿⡟');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀     ⠀⠀⠀⠀⠀⠀⢻⡟');
+
+
+$writer->br();
+$writer->writeLine('AI Star:');
+$writer->br();
+
+$writer->write($format->fg_bright_yellow);
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⣀⣠⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣀⣀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠶⠶⠶⠾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡶⠶⠶⠶');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠈⠉⠛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠉⠁⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->writeLine('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀');
+$writer->write($format->reset);
+
+$writer->br();
+$writer->writeLine('Pith Framework logo');
+$writer->br();
+
+$writer->write($format->fg_dark_green);
+$writer->writeLine('⠀⠀⠀⣠⢴⡲⣫⢏⡟⣽⡹⣝⢽⡹⣝⣝⢝⣝⢝⡝⣭⢫⡝⣭⢫⢝⢭⡫⠀⢸⡩⣫⡙⣕⠲⡢⣀⠀⠀⠀');
+$writer->writeLine('⠀⢠⣞⢵⡳⣝⢮⡳⣝⢮⡺⡪⡧⣫⢖⢵⢝⡜⡵⡹⣜⢎⢞⠴⡳⡱⡳⠀⢠⡳⡱⢕⢎⢮⡙⣜⢌⡳⠄⠀');
+$writer->writeLine('⢰⢯⡺⣕⢯⡺⣕⢯⣪⢳⢝⢮⡳⣕⢽⡱⣝⣜⢝⢮⡪⣣⢏⡝⣎⢧⠋⢀⢎⢮⢪⡣⣓⢕⢎⢎⢎⢎⢝⡂');
+$writer->writeLine('⣯⡳⣝⡮⣳⢝⡮⣳⢕⢯⡳⡵⣝⢼⢕⡵⡣⡮⡳⣕⢵⢕⡵⡹⡔⡇⠀⣜⢎⠶⡱⡕⣕⢕⠵⡱⡕⣕⢕⢕');
+$writer->writeLine('⣧⡻⣜⡽⣪⢗⡽⣪⡳⣣⢯⡺⣜⢵⡣⣏⢞⢮⢺⢜⢮⢪⢞⢜⡎⠀⡸⡪⣪⢓⢭⢪⢆⢏⠮⡪⡪⡢⣣⠙');
+$writer->writeLine('⣷⣫⢞⡮⣳⢝⡮⡳⣝⢮⡺⣪⢎⡗⡵⣣⢏⣞⢕⢗⢵⡣⣫⢎⠁⡰⡹⡪⡎⡵⡣⣓⠵⡩⣎⠵⡩⠎⠀⠀');
+$writer->writeLine('⣟⢮⡳⣝⢮⡳⣝⢮⡳⣕⢯⡪⣗⢝⢮⡺⡪⣎⢗⢝⢮⢪⢞⠀⣠⢏⢞⢜⢎⢮⢪⢎⡕⡳⡘⠊⠀⠀⣀⢴');
+$writer->writeLine('⣗⢯⡞⣵⡫⣞⢵⡫⡞⣮⢣⢯⡪⣳⢣⡏⡾⡸⣕⢏⢮⢳⠁⣠⢣⡳⣍⢮⢓⠵⡱⡣⠎⠁⠀⢀⢤⡚⡴⡱');
+$writer->writeLine('⡯⣺⢝⡮⣳⢝⡮⣳⢝⢮⡳⣕⢯⡪⡧⣫⢺⢕⡵⡹⣪⠁⣠⢎⢗⢜⢦⢳⡩⠓⠉⠀⢀⢤⢪⡓⣕⢪⠪⡺');
+$writer->writeLine('⣟⢮⡳⣝⢮⡳⣝⢮⡳⣣⢏⡞⡮⡺⡪⡧⣫⢞⢼⡱⠁⡰⣕⢕⢏⢮⠃⠃⠀⢀⡤⣪⢣⡣⡣⢳⢌⡣⡫⢜');
+$writer->writeLine('⣏⢷⢝⣮⢳⡝⣮⢳⢝⢮⡳⣝⢎⡟⡼⣪⢞⡜⡵⠁⡰⡳⡜⠕⠋⠀⢀⢤⡚⣕⢕⡕⡕⢮⡱⡣⡣⣕⢍⡺');
+$writer->writeLine('⣿⢕⡯⣺⢕⡯⣺⢕⡯⡺⣎⢮⡳⣹⡪⡳⣕⠝⢀⠜⠎⠁⠀⠀⠤⠲⠙⠂⠙⣈⢊⡈⣉⢠⢄⡤⡠⡄⡤⣀');
+$writer->writeLine('⣏⢷⡝⣮⡳⣝⢮⡳⣝⢮⡳⣕⢯⡪⣞⢕⠃⠀⠁⠀⢀⡠⡦⡲⣒⢵⡩⣫⢫⡪⡕⡕⡵⡱⡱⡪⡪⡪⡪⢼');
+$writer->writeLine('⡗⡷⣹⢎⡷⣹⢎⢷⡱⡧⣫⡺⣪⠞⠈⠁⠀⠀⣠⢖⡫⢞⡜⡵⡱⡕⡵⡱⡣⡺⣘⠮⡪⣪⢪⢎⠮⡪⡪⣪');
+$writer->writeLine('⣏⣟⢼⡳⣝⢮⡳⣫⢞⠵⠃⠋⠀⠀⠀⣠⢴⢫⢞⡜⣕⢏⢞⡜⡵⣙⠮⡪⡳⡩⣎⢣⡫⡲⡱⡱⢍⠮⡪⢜');
+$writer->writeLine('⣗⢽⡪⣗⢽⡪⠗⠉⠈⠀⠀⠀⠀⠴⠺⠕⠗⠵⠣⠏⠮⠣⠫⠺⡜⣎⢳⡙⣎⢮⢪⢕⢕⢕⢕⢝⢬⢣⠝⢼');
+$writer->writeLine('⢣⡟⠎⠃⠁⠀⠀⠀⠀⠀⢀⣀⣀⣠⡠⢤⠤⡤⢤⢄⢤⢄⡤⣀⣀⣀⡈⡈⠈⠀⠓⠑⠩⠪⠣⠣⣣⢣⡋⡞');
+$writer->writeLine('⠈⠀⠀⠀⠀⠀⠀⣠⢖⢟⡝⡮⡵⣕⢝⢮⡳⡹⡪⡳⡕⡧⡳⡕⡵⡪⣎⢝⢭⢫⡚⡲⡒⢦⢢⠤⣠⣀⣀⠁');
+$writer->writeLine('⠀⠐⠀⠀⢀⡤⡾⣱⡫⡳⣝⢞⢮⣪⡳⣣⢏⡞⣝⠮⡺⡪⡞⣜⢮⢪⢎⢮⢣⡣⣝⢜⢕⡣⡳⡱⢕⡔⠃⠀');
+$writer->writeLine('⠀⠀⠀⠁⠛⠼⢝⣮⣝⣵⡳⣝⡮⣖⣝⣮⡺⣜⣎⣏⣞⣕⣝⡼⣌⣗⣕⣇⣳⣱⣪⣪⣕⠕⠵⠑⠉⠀⠀⠀');
+$writer->write($format->reset);
+
+$writer->br();
+$writer->br();
+$writer->br();
+
+
+
+
