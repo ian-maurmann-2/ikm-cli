b8226f371cf1ba45fa949b02cfe5ff5369346a91
Ian Maurmann <ikmxx@yahoo.com>
2024-09-23 19:17:00 -0700

    Add test spacers

---
+++ test-0012-spacers.php
@@ -0,0 +1,40 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+require 'vendor/autoload.php';
+
+$writer = new \IKM\CLI\CommandLineWriter();
+$format = new \IKM\CLI\CommandLineFormatter();
+
+// ===========================================
+
+
+
+$writer->br();
+$writer->br();
+$writer->br();
+
+$writer->writeLine('─────────────────────────────────────────────────────');
+
+
+$writer->br();
+$writer->br();
+$writer->br();
+
+$writer->writeLine('                      │                      ');
+$writer->writeLine('                      │                      ');
+$writer->writeLine('───────────────╮      │      ╭───────────────');
+$writer->writeLine('               ╰──────┴──────╯               ');
+
+
+
+
+$writer->br();
+$writer->br();
+$writer->br();
+
+$writer->writeLine('                  │                  ');
+$writer->writeLine('                  ┃                  ');
+$writer->writeLine('───────────────╮  │  ╭───────────────');
+$writer->writeLine('               ╰──┴──╯               ');
+
